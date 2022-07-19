d59e0172733e48ffddfef6f0bbb71071d49ef9a1
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-19 10:43:30 +0000

    Fix: Add seeder for feed

--- src/seeder.ts
+++ src/seeder.ts
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 import users from "./data/users";
 import gists from "./data/gists";
 import posts from "./data/posts";
+import feed from "./data/feed";
 import User from "./models/User";
 import Post from "./models/Post";
 import connectDB from "./lib/db";
 import Gist from "./models/Gist";
+import Feed from "./models/Feed";
 
 dotenv.config();
 
@@ -18,6 +20,7 @@ const importData = async () => {
     await Post.deleteMany();
     await User.deleteMany();
     await Gist.deleteMany();
+    await Feed.deleteMany();
     const createdUsers = await User.insertMany(users);
 
     const adminUser = createdUsers[0]._id;
@@ -30,9 +33,12 @@ const importData = async () => {
       return { ...post, author: adminUser };
     });
 
+    const sampleFeed = feed.map((feat) => {
+      return { ...feat, author: adminUser };
+    });
     await Gist.insertMany(sampleGists);
     await Post.insertMany(samplePosts);
-
+    await Feed.insertMany(sampleFeed);
     console.log("Data Imported!");
     process.exit();
   } catch (error) {
