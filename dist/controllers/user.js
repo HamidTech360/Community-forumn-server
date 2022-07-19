a54125e63872b2ab23c7c1f71461218cf3eafacd
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-19 08:05:40 +0000

    Fix: Inconsistent ts-build name

---
+++ dist/controllers/user.js
@@ -0,0 +1,64 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateUser = exports.getUser = exports.getUsers = void 0;
+const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const User_1 = __importDefault(require("../models/User"));
+const fs_1 = __importDefault(require("fs"));
+//@route: /api/users
+//@method: GET
+//@access: public
+exports.getUsers = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const users = yield User_1.default.find();
+        res.status(200).json(users);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}));
+//@route: ./api/users/:id
+//@method: GET
+//@access public
+exports.getUser = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const user = yield User_1.default.findById(req.params.id).populate("followers");
+        res.status(200).json(user);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}));
+exports.updateUser = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.file);
+    if (req.file) {
+        const fileType = req.file.mimetype.split("/")[1];
+        const rename = `${req.file.filename}.${fileType}`;
+        fs_1.default.rename(`./uploads/${req.file.filename}`, `./uploads/${rename}`, function () {
+            //res.send('uploaded successfully')
+            //response.imgUploaded = true
+        });
+    }
+    //return
+    try {
+        const user = yield User_1.default.findByIdAndUpdate(req.params.id, Object.assign({}, req.body));
+        res.json({
+            status: "success",
+            message: "User updated",
+        });
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}));
