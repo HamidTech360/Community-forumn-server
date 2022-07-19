29a79436c0131d9f2842e09015b1c83c05a11ab8
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-19 07:23:00 +0000

    Fix: Bookmark populate (Raw)

--- src/controllers/bookmark.ts
+++ src/controllers/bookmark.ts
@@ -1,55 +1,60 @@
+import { Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
-import User from "../models/User";
+import { AnyKeys } from "mongoose";
+import User, { IUserSchema } from "../models/User";
 
 export const AddToBookMark = expressAsyncHandler(
-    async (req:any, res:any)=>{
-        const {id} = req.query
-        console.log(id, req.user?._id);
-        
-        try{
-            await User.findByIdAndUpdate(req.user?._id, {
-                $addToSet:{
-                    bookmarks:id
-                }
-            })
-            res.json({
-                message:'Post bookmarked'
-            })
-        }catch(error){
-            res.status(500).send(error)
-        }
+  async (req: Request & { user?: AnyKeys<IUserSchema> }, res: Response) => {
+    const { id } = req.query;
+    console.log(id, req.user?._id);
+
+    try {
+      await User.findByIdAndUpdate(req.user?._id, {
+        $addToSet: {
+          bookmarks: id,
+        },
+      });
+      res.json({
+        message: "Post bookmarked",
+      });
+    } catch (error) {
+      res.status(500).send(error);
     }
-)
+  }
+);
 
 export const RemoveFromBookmark = expressAsyncHandler(
-    async (req:any, res:any)=>{
-        const {id}= req.query
-        try{
-            await User.findByIdAndUpdate(req.user?._id, {
-                $pull:{bookmarks:id}
-            })
-            res.json({
-                message:'Post removed from bookmarks'
-            })
-        }catch(error){
-            res.status(500).send(error)
-        }
+  async (req: Request & { user?: AnyKeys<IUserSchema> }, res: Response) => {
+    const { id } = req.query;
+    try {
+      await User.findByIdAndUpdate(req.user?._id, {
+        $pull: { bookmarks: id },
+      });
+      res.json({
+        message: "Post removed from bookmarks",
+      });
+    } catch (error) {
+      res.status(500).send(error);
     }
-)
+  }
+);
 
 export const getBookMarks = expressAsyncHandler(
-    async (req:any, res:any)=>{
-        try{
-            const posts =  await User.findById(req.user._id)
-                                .select({bookmarks:1})
-                                .populate('bookmarks')
-                                
-            res.json({
-                message:'Bookmarks fetched',
-                posts
-            })
-        }catch(error){
-            res.status(500).send(error)
-        }
+  async (req: Request & { user?: AnyKeys<IUserSchema> }, res: Response) => {
+    try {
+      const posts = await User.findById(req.user?._id)
+        .select("bookmarks")
+        .populate({
+          path: "bookmarks",
+          populate: { path: "author", select: "firstName lastName avatar" },
+        });
+
+      res.json({
+        message: "Bookmarks fetched",
+        posts,
+      });
+    } catch (error) {
+      res.status(500).send(error);
     }
-)
+  }
+);
