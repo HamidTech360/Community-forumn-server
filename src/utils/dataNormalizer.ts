879ff60946a43312496d753545aee6c471a356f3
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-23 10:31:47 +0000

    Feat: Add groups to search

--- src/utils/dataNormalizer.ts
+++ src/utils/dataNormalizer.ts
@@ -1,10 +1,10 @@
 export const normalizeGoogleData = async (data: any) => {
   return {
     email: data.email,
-    fullName: data.name,
-    picture: data.imageUrl || null,
+    firstName: data.givenName,
+    lastName: data.familyName,
+    avatar: data.imageUrl || null,
     authProvider: data.googleId && "GOOGLE",
-    role: data.role,
   };
 };
 
