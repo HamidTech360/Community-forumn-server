c26b8aaa28566851be27ccca264916a1c63f90ea
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-07-25 12:13:44 +0000

    Fix: Readd followers

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import * as controller from "../controllers/user";
+import { loggedIn } from "../middleware/auth";
 // import multer from 'multer'
 //const upload = multer({dest:"./uploads/"})
 const router = express.Router();
@@ -8,5 +9,5 @@ router.get("/", controller.getUsers);
 
 router.get("/:id", controller.getUser);
 router.put("/:id", controller.updateUser);
-
+router.get("/:id/follow", loggedIn, controller.followUser);
 export default router;
