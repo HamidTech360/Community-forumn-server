b9605c677c81183840c1f462e1f09e9c2d668948
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-08-13 10:49:30 +0000

    Chore: build ts

--- dist/controllers/feed.js
+++ dist/controllers/feed.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateFeeds = exports.getRandomGroupFeed = exports.getGroupFeed = exports.fetchFeed = exports.fetchFeeds = exports.saveFeed = void 0;
+exports.updateFeed = exports.getRandomGroupFeed = exports.getGroupFeed = exports.fetchFeed = exports.fetchFeeds = exports.saveFeed = void 0;
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const Feed_1 = __importDefault(require("../models/Feed"));
 const notification_1 = __importDefault(require("../models/notification"));
@@ -185,7 +185,7 @@ exports.getRandomGroupFeed = (0, express_async_handler_1.default)((req, res) => 
 //@Route /api/feed/:id
 //@Method Put
 //@Access: LoggedIn
-exports.updateFeeds = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.updateFeed = (0, express_async_handler_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _f;
     const feedId = req.params.id;
     const feed = yield Feed_1.default.findById(feedId).where({
